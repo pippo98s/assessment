f8eca6fa6a7dfeb1b999f75c29a2828b554e8110
pippo98s <filippo5scarcella@gmail.com>
2020-10-07 11:27:57 +0200

    inizio test

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader : "html-loader",
+            // options: {minimize: true}
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devtool: "inline-source-map"
+}
